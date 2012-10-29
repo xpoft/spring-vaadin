205dd6685582330e2f14bd32bc4ba99c7d2edc54
Alexander Fedorov <xpoft@xpoft.ru>
2012-10-29 23:17:31 +0400

    add support @RequiresPermission

--- src/main/java/ru/xpoft/vaadin/security/ShiroSecurityNavigator.java
+++ src/main/java/ru/xpoft/vaadin/security/ShiroSecurityNavigator.java
@@ -4,6 +4,8 @@ import com.vaadin.navigator.View;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.UI;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
@@ -14,7 +16,7 @@ import ru.xpoft.vaadin.SpringApplicationContext;
 import java.util.*;
 
 /**
- * Uses RequiresRoles annotation. You don't need AspectJ
+ * Uses @RequiresRoles and @RequiresPermissions annotations. You don't need AspectJ
  * Exclude views, that user doesn't have access
  *
  * @author xpoft
@@ -55,20 +57,77 @@ public class ShiroSecurityNavigator extends DiscoveryNavigator
 
     private boolean hasAccess(Class<?> clazz)
     {
+        boolean isAllow = true;
+
         if (clazz.isAnnotationPresent(RequiresRoles.class))
         {
+            isAllow = false;
+
             RequiresRoles requiresRoles = clazz.getAnnotation(RequiresRoles.class);
             String[] roles = requiresRoles.value();
+            Logical logical = requiresRoles.logical();
             if (roles.length > 0)
             {
                 Subject subject = SecurityUtils.getSubject();
-                if (!subject.isAuthenticated() || !subject.hasAllRoles(Arrays.asList(roles)))
+                if (!subject.isAuthenticated())
                 {
                     return false;
+                }
+
+                if (logical == Logical.AND && subject.hasAllRoles(Arrays.asList(roles)))
+                {
+                    isAllow = true;
+                }
+
+                if (logical == Logical.OR)
+                {
+                    for (boolean hasRole : subject.hasRoles(Arrays.asList(roles)))
+                    {
+                        if (hasRole)
+                        {
+                            isAllow = true;
+                            break;
+                        }
+                    }
                 }
             }
         }
 
-        return true;
+        if (isAllow && clazz.isAnnotationPresent(RequiresPermissions.class))
+        {
+            isAllow = false;
+
+            RequiresPermissions requiresPermissions = clazz.getAnnotation(RequiresPermissions.class);
+            String[] permissions = requiresPermissions.value();
+            Logical logical = requiresPermissions.logical();
+            Subject subject = SecurityUtils.getSubject();
+
+            if (permissions.length > 0)
+            {
+                if (!subject.isAuthenticated())
+                {
+                    return false;
+                }
+
+                if (logical == Logical.AND && subject.isPermittedAll(permissions))
+                {
+                    isAllow = true;
+                }
+
+                if (logical == Logical.OR && subject.isPermittedAll(permissions))
+                {
+                    for (boolean isPermitted : subject.isPermitted(permissions))
+                    {
+                        if (isPermitted)
+                        {
+                            isAllow = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return isAllow;
     }
 }
